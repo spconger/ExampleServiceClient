59661cf4b8b6002532e23721c6c71d7732d60a95
Steve Conger <spconger@gmail.com>
2016-02-01 11:00:38 -0800

    CommitExampleClient212016

---
+++ Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:DropDownList ID="DropDownListAuthor" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownListAuthor_SelectedIndexChanged"></asp:DropDownList>
+        <asp:GridView ID="GridViewBooks" runat="server"></asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    BookReviewServiceReference.AuthorBookServiceClient db 
+        = new BookReviewServiceReference.AuthorBookServiceClient();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            LoadDropDown();
+        }
+    }
+    protected void DropDownListAuthor_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        FillGrid();
+    }
+
+    protected void LoadDropDown()
+    {
+        string[] authors = db.GetAuthors();
+        DropDownListAuthor.DataSource = authors;
+        DropDownListAuthor.DataBind();
+        DropDownListAuthor.Items.Insert(0, "Choose an Author");
+    }
+
+    protected void FillGrid()
+    {
+        string author = DropDownListAuthor.SelectedItem.Text;
+        BookReviewServiceReference.BookLite[] books= db.GetBooks(author);
+        GridViewBooks.DataSource = books;
+        GridViewBooks.DataBind();
+    }
+}
